03cd99ed502d840fa5b9188fb028022a4cce7903
Jw <xs9562@gmail.com>
2023-09-07 18:09:30 +0900

    Changes

---
+++ src/main/java/note/P_note/PNoteApplication.java
@@ -0,0 +1,13 @@
+package note.P_note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PNoteApplication.class, args);
+	}
+
+}
